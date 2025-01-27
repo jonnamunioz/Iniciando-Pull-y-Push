0b3b30252f7cfbdfca4238d258b84ab6abe6f59a
Jonna muñoz <jonathan_munioz@hotmail.com>
2025-01-27 15:44:45 -0300

    Segundo-Commit

---
+++ prueba.py
@@ -0,0 +1,27 @@
+"""
+Un programa que de un objeto simple
+En este caso el programa imprimirá un número
+"""
+print("Hola, este es un programa que solo saluda")
+
+"""
+Pedirle a un usuario el valor de un número para guardar (su correo). 
+Este número que se guarda, usualmente tiene un valor de
+relevancia para nosotros. 
+"""
+
+email = input('Ingresa tu email: ')
+print('Tu email es ',email)
+
+input: ("hola mundo")
+
+
+"""
+Sacar promedio de 3 notas
+"""
+
+Nota1 = int(input('Ingrese nota 1 : '))
+Nota2 = int(input('Ingrese nota 2 : '))
+Nota3 = int(input('Ingrese nota 3 : '))
+Promedio = (Nota1+Nota2+Nota3)/3
+print(Promedio)
